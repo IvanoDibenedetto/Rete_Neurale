f0c689428941880fb2f93a53f2c4931d7697080e
Ivano Dibenedetto <38322663+IvanoDibenedetto@users.noreply.github.com>
2020-09-30 16:53:11 +0200

    Add files via upload

---
+++ NeuralNetwork.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 28 00:01:25 2020
+
+@author: Ivano Dibenedetto mat. 654678
+"""
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+
+import pickle
+
+pickle_in = open("X.pickle","rb")
+X = pickle.load(pickle_in)
+
+pickle_in = open("y.pickle","rb")
+y = pickle.load(pickle_in)
+
+X = np.array(X/255.0)
+y = np.array(y)
+
+
+model = Sequential()
+
+model.add(Conv2D(64, (3, 3), input_shape=X.shape[1:]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+
+model.add(Dense(64))
+
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+model.fit(X, y, batch_size=32, epochs=5, validation_split=0.2)
+model.save("X-ray_CNN.model")
---
+++ PreProcessing.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 27 09:32:14 2020
+
+@author: Ivano Dibenedetto mat. 654678
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+from tqdm import tqdm
+import random
+import pickle
+
+
+DATADIR = "C:/Users/Kiwi/Desktop/UNIBA/sistemi multimediali/Progetto covid19/Covid19_dataset_global"
+
+CATEGORIES = ["Covid","Polmonite"]
+IMG_SIZE = 450
+training_data = []
+
+
+def create_training_data():
+    
+    for category in CATEGORIES:
+        path = os.path.join(DATADIR,category)
+        class_num = CATEGORIES.index(category)
+        
+        for img in tqdm(os.listdir(path)):
+            img_array = cv2.imread(os.path.join(path,img), cv2.IMREAD_GRAYSCALE)
+            new_array = cv2.resize(img_array,(IMG_SIZE, IMG_SIZE))
+            training_data.append([new_array,class_num])
+
+
+create_training_data()
+
+print(len(training_data))
+random.shuffle(training_data)
+
+X = []
+y = []
+
+
+for features,label in training_data:
+    X.append(features)
+    y.append(label)
+    
+X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+    
+pickle_out = open("X.pickle","wb")
+pickle.dump(X, pickle_out)
+pickle_out.close()
+
+pickle_out = open("y.pickle","wb")
+pickle.dump(y, pickle_out)
+pickle_out.close()
