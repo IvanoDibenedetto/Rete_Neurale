7fbd57098c735344a2ca5d104fd777004530fc31
Ivano Dibenedetto <38322663+IvanoDibenedetto@users.noreply.github.com>
2020-10-13 18:28:49 +0200

    Add files via upload

---
+++ testModel.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 28 08:48:10 2020
+
+@author: Ivano Dibenedetto mat.654678
+"""
+
+
+
+import tensorflow as tf
+import cv2
+import os
+from os import path
+from tqdm import tqdm
+
+
+CATEGORIES = ["Covid", "Polmonite"]  
+DATADIR = "C:/Users/Kiwi/Desktop/UNIBA/sistemi multimediali/Progetto covid19/Test/"
+
+def prepare(filepath):
+    IMG_SIZE = 250
+    img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE) 
+    new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE)) 
+    return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+
+model = tf.keras.models.load_model("X-ray_CNN.model")
+
+for img in (os.listdir(DATADIR)):
+    name, extension = path.splitext(img)
+    prediction = model.predict([prepare(path.join(DATADIR,img))])
+    cv2img = cv2.imread(path.join(DATADIR,img),cv2.IMREAD_GRAYSCALE)
+    print("Categoria: ",name,"==",CATEGORIES[int(prediction[0])])
+   
